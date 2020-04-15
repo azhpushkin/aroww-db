157be2f7a6c3fe0befde2e76189e95247af15969
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2020-04-16 00:25:15 +0300

    Improve client.cpp (drop fgets and sscanf)

--- client.cpp
+++ client.cpp
@@ -1,5 +1,47 @@
 #include <fmt/format.h>
+#include <regex>
+#include <iostream>
+#include <vector>
 #include "lib/aroww.hpp"
+
+
+class Command {
+public:
+    std::string name;
+    std::vector<std::string> args;
+};
+
+
+std::vector<std::string> split(std::string s) {
+    std::vector<std::string> vec;
+
+    size_t pos = 0;
+    std::string token;
+    std::string delimeter = " ";
+    while ((pos = s.find(delimeter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        vec.push_back(s.substr(0, pos));
+        s.erase(0, pos + delimeter.length());
+    }
+    vec.push_back(s.substr(0, pos));
+
+    return vec;
+}
+
+
+Command parse_input(std::string input) {
+    std::vector<std::string> args = split(input);
+    
+    Command com;
+    com.name = args.at(0);
+    std::transform (com.name.begin(), com.name.end(), com.name.begin(), ::tolower);
+
+    args.erase(args.begin());
+    com.args = args;
+
+    return com;
+}
+
 
 
 int main(int argc, char *argv[])
@@ -8,41 +50,55 @@ int main(int argc, char *argv[])
 	ArowwResult res;
 
 	conn.open_conn();
+    std::string user_input;
 
-	int sscanfres;
-    char key[128];
-    char value[128];
-    char input[128];
-
-    while(1) {
-
-        std::cout << "cli> " << std::flush;
-		fgets(input, 128, stdin);
-
-        for (char* p = input; *p; ++p)
-            *p = tolower(*p);
-
-        if ((sscanfres = sscanf(input, "get %s", key)) == 1) {
-			res = conn.get(std::string(key));
-        } else if ((sscanfres = sscanf(input, "drop %s", key)) == 1) {
-			res = conn.drop(std::string(key));
-        } else if ((sscanfres = sscanf(input, "set %s %s", key, value)) == 2) {
-			res = conn.set(std::string(key), std::string(value));
-        } else if ((sscanfres = sscanf(input, "q")) == 1) {
-			std::cout << "Quitting!.." << std::endl;
-            break;
-        } else {
-			sscanf(input, "%s", input);  // truncate
-			std::cout << "Command " << input << " is unknown" << std::endl;	
+    std::cout << "cli> " << std::flush;
+    while(std::getline(std::cin, user_input)) {
+        // Trim start and end whitespaces
+        user_input = std::regex_replace(user_input, std::regex("^ +| +$|( ) +"), "$1");
+        if (!user_input.length()) {
+            std::cout << "cli> " << std::flush;
             continue;
         }
 
-		if (res.success) {
+        Command com = parse_input(user_input);
+        if (com.name == "get") {  // GET
+            if (com.args.size() != 1) {
+                std::cout << "Err: expected 1 argument for get, got "<< com.args.size() << std::endl;
+                std::cout << "cli> " << std::flush;
+                continue;
+            }
+            res = conn.get(com.args.at(0));
+        } else if (com.name == "drop") {  // DROP
+            if (com.args.size() != 1) {
+                std::cout << "Err: expected 1 argument for drop, got "<< com.args.size() << std::endl;
+                std::cout << "cli> " << std::flush;
+                continue;
+            }
+            res = conn.drop(com.args.at(0));
+        } else if (com.name == "set") {  // SET
+            if (com.args.size() != 2) {
+                std::cout << "Err: expected 2 argument for set, got "<< com.args.size() << std::endl;
+                std::cout << "cli> " << std::flush;
+                continue;
+            }
+            res = conn.set(com.args.at(0), com.args.at(1));
+        } else if (com.name == "q") {
+            break;
+        } else {
+            std::cout << "Command " << com.name << " is unknown!" << std::endl; 
+            std::cout << "cli> " << std::flush;
+            continue;
+        }
+
+        if (res.success) {
 			std::cout << fmt::format("OK: {}", res.value.has_value() ? res.value.value() :"<empty>") << std::endl;
 		} else {
 			std::cout << fmt::format("ERR: {}", res.error_msg.value()) << std::endl;
-		}
-        
+		} 
+        std::cout << "cli> " << std::flush;
     }
 
+
+    std::cout << "Quitting!.." << std::endl;
 }
